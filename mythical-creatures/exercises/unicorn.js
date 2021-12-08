429776fa2fbd7efa91d17bf1e2463e0c440cb098
Rio Foster <friotious@gmail.com>
2021-12-08 14:40:24 -0800

    Complete unicorn.js tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,42 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
   }
-}
+  isWhite() {
+    if (this.color != 'white') {
+      return false
+    }
+  }
+    says(words) {
+      return `**;* ${words} *;**`
+    }
+    //   if (this.color.length === 0) {
+    //     this.color.replace('white', color)
+    //   }
+    }
+
+
+
+
+//this.color = color
+//
+//this.color.replace('white', color)
+//
+// if (color != 'white') {
+// this.color.push(color)
 
 module.exports = Unicorn;
+
+
+// class Unicorn {
+//   constructor(name, color) {
+//     this.name = name;
+//     this.color = 'white'
+//   }
+//     isWhite(color) {
+//       if (color.length < 1) {
+//       this.color = color
+//       }
+//     }
+// }
